3da0b742b148d0e54ae6856c534e72922dc2bbd3
betel <betelgeuse525@gmail.com>
2012-12-21 20:44:03 +0900

    solved:add:id0020,in progress:add:id0021

---
+++ AOJ/vol.00/id0020/Main.java
@@ -0,0 +1,13 @@
+package id0020;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner	sc	= new Scanner(System.in);
+		String	s	= sc.nextLine();
+		sc.close();
+		
+		System.out.println(s.toUpperCase());
+	}
+}
---
+++ AOJ/vol.00/id0021/Main.java
@@ -0,0 +1,40 @@
+package id0021;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		String lineStr;
+		double ex1,ey1,ex2,ey2;
+		ArrayList<String> list = new ArrayList<String>();
+
+		Scanner sc = new Scanner(System.in);
+		while((lineStr=sc.nextLine())!=null){
+			list.add(lineStr);
+		}
+		sc.close();
+
+		int size = list.size();
+		double[] num	= new double[size];
+		String[] s		= new String[size];
+		for(int i=1; i<size; i++){
+			s = list.get(i).split(" ");
+
+			for(int j=0; j<s.length; j++){
+				num[j] = Double.parseDouble(s[j]);
+			}
+
+			ex1 = num[0]-num[2];
+			ey1 = num[1]-num[3];
+			ex2 = num[4]-num[6];
+			ey2 = num[5]-num[7];
+
+			System.out.println(isParallel(ex1,ey1,ex2,ey2));
+		}
+	}
+
+	public static boolean isParallel(double ex1,double ey1,double ex2,double ey2){
+
+	}
+}
