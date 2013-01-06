ed0ee1e8a5c911f37c0577447d13ce83b8a5754d
betel <betelgeuse525@gmail.com>
2013-01-06 23:26:20 +0900

    OPEN - task 2: 面積と高さが計算されない。周の長さも精度が悪い http://github.com/betel/AOJ/issues/issue/2

--- AOJ/vol.100/id10025/Main.java
+++ AOJ/vol.100/id10025/Main.java
@@ -1,42 +1,65 @@
 package id10025;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Main {
-	
-	ArrayList<String> list;
-	double a,b,c,s;
 
-	public Main(){
+	ArrayList<String> list;
+	ArrayList<String[]> strList;
+
+	public Main(){	//コンストラクタ
 		list = new ArrayList<String>();
+		strList = new ArrayList<String[]>();
+		init();
 	}
-	public void Read(){
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	//
+	public void start(){
+		for(int i=0; i<strList.size(); i++){
+			String[] str = strList.get(i);
+			double a = Double.parseDouble(str[0]);
+			double b = Double.parseDouble(str[1]);
+			double c = Double.parseDouble(str[2]);
+
+			System.out.println(calcS(a,b,c));
+			System.out.println(calcL(a,b,c));
+			System.out.println(calcH(a,b,c));
+		}
+	}
+	//初期化
+	public void init(){
+		BufferedReader br;
 		try {
+			br = new BufferedReader(new FileReader("sampleData/id10025D.txt"));
 			String str;
-			while((str=br.readLine())!=null){
+			while((str=br.readLine())!=null){	//1行毎にlistに追加
 				list.add(str);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		for(int i=0; i<list.size(); i++){
-			String[] strList;
-			strList = list.get(i).split(" ");
-			a = Double.parseDouble(strList[0]);
-			b = Double.parseDouble(strList[1]);
-			c = Double.parseDouble(strList[2]);
+		for(int i=0; i<list.size(); i++){		//1行を空白で分けて作成した配列をすべてstrListに入れる
+			strList.add(list.get(i).split(" "));
 		}
-		
 	}
-	public void calc(double a, double b, double c){
-		s = 1/2 * a * b * Math.sin(c);
+	//計算
+	public double calcS(double a, double b, double c){
+		return	1/2 * a * b * Math.sin(c);
 	}
-	
+	public double calcL(double a, double b, double c){
+		double cc		= a*a + b*b -2*a*b*Math.cos(c);
+		double sideC 	= Math.sqrt(cc);
+		return	a + b + sideC;
+	}
+	public double calcH(double a, double b, double c){
+		double s	= calcS(a,b,c);
+		return	2*s / a;
+	}
+	//実行
 	public static void main(String[] args){
-		new Main().calc();
+		Main main = new Main();
+		main.start();
 	}
 }
