0416c818c8603cfc56aa18f96114f23c9e30a3ae
betel <betelgeuse525@gmail.com>
2013-01-07 03:27:50 +0900

    OPEN - task 3: at id10022:パターンマッチングを使う http://github.com/betel/AOJ/issues/issue/3

--- AOJ/vol.100/id10022/Main.java
+++ AOJ/vol.100/id10022/Main.java
@@ -1,43 +1,53 @@
 package id10022;
 
+/**
+ * 大文字小文字を区別しない！
+ */
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException{
-		ArrayList<String> list,words;
+	ArrayList<String> list,words;
+	BufferedReader br;
+	int count = 0;
+
+	public Main(){
+		list = new ArrayList<String>();
+		words = new ArrayList<String>();
+	}
+
+	public void start(){
 		String str;
 		String[] w;
-		int count = 0;
-		BufferedReader br;
-		br = new BufferedReader(new FileReader("id10022data.txt"));
 		try {
-			list = new ArrayList<String>();
-			while((str=br.readLine())!="END_OF_TEXT"){
+			br = new BufferedReader(new FileReader("sampleData/id10022D.txt"));
+			while(!(str=br.readLine()).equals("END_OF_TEXT")){
 				list.add(str);		//1行ごとにlistに追加
 			}
-
-			words = new ArrayList<String>();
-			for(int i=0; i<list.size(); i++){
-				w = list.get(i).split(" ");
-				for(String s : w){
-					words.add(s);
-				}
-			}
-			//判別
-			String sample = words.get(0);
-			for(String s : words){
-				if(sample.equals(s)){
-					count++;
-				}
-			}
-			System.out.println(count-1);
-		} catch (IOException e) {
+		}catch(IOException e){
 			e.printStackTrace();
 		}
+
+		for(int i=0; i<list.size(); i++){	//すべての単語をwordsに追加
+			w = list.get(i).split(" ");
+			for(String s : w){
+				words.add(s);
+			}
+		}
+		//判別
+		String sample = words.get(0);	//wordsの1番目(=W)をsampleに代入
+		for(String s : words){
+			if(sample.equals(s)){		//sampleと一致した回数をカウント
+				count++;
+			}
+		}
+		System.out.println(count-1);	//1番目(=W)も一致してしまうので1引く
+	}
+
+	public static void main(String[] args){
+		new Main().start();
 	}
 }
