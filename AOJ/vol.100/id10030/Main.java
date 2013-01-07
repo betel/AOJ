fcccfb88a9076fe5c221ad9dd6f7d9e98c156be3
betel <betelgeuse525@gmail.com>
2013-01-07 16:51:09 +0900

    OPEN - task 4: id10030:WA http://github.com/betel/AOJ/issues/issue/4

--- AOJ/vol.100/id10030/Main.java
+++ AOJ/vol.100/id10030/Main.java
@@ -1,34 +1,51 @@
 package id10030;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Main {
 	ArrayList<String>	list = new ArrayList<String>();
 	int[] numList1,numList2;
+	int count = 0;
+
 
 	public void start(){
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str;
 		try {
+			BufferedReader br = new BufferedReader(new FileReader("sampleData/id10030D.txt"));
 			while((str=br.readLine())!=null){
-				list.add(str);
+				list.add(str.trim());
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		numList1 = new int[Integer.parseInt(list.get(0))];
-		numList2 = new int[Integer.parseInt(list.get(2))];
+		int fact1 = Integer.parseInt(list.get(0));
+		int fact2 = Integer.parseInt(list.get(2));
+		numList1 = new int[fact1];
+		numList2 = new int[fact2];
+		String[] s1	=	list.get(1).split(" ");
+		String[] s2 =	list.get(3).split(" ");
 
-		for(int i=0; i<numList1.length; i++){
-			String[] s1	=	list.get(1).split(" ");
-			String[] s2 =	list.get(3).split(" ");
+		for(int i=0; i<fact1; i++){
 			numList1[i] = Integer.parseInt(s1[i]);
+		}
+		for(int i=0; i<fact2; i++){
 			numList2[i] = Integer.parseInt(s2[i]);
 		}
+
+		for(int i=0; i<fact1; i++){
+			for(int j=0; j<fact2; j++){
+				if(numList1[i] == numList2[j]){
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
 	}
 	public static void main(String[] args){
 		new Main().start();
