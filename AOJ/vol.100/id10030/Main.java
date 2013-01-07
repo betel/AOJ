21454bb8bcd27f481ecc6b778014914d6b68faf4
betel <betelgeuse525@gmail.com>
2013-01-07 19:03:00 +0900

    OPEN - task 4: id10030:重複を許してはいけない http://github.com/betel/AOJ/issues/issue/4

--- AOJ/vol.100/id10030/Main.java
+++ AOJ/vol.100/id10030/Main.java
@@ -6,17 +6,23 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
-	ArrayList<String>	list = new ArrayList<String>();
-	int[] numList1,numList2;
+	ArrayList<String>	list;
+	ArrayList<Integer>	numList1,numList2,blackList;
 	int count = 0;
+
+	public Main(){
+		list = new ArrayList<String>();
+		numList1 = new ArrayList<Integer>();
+		numList2 = new ArrayList<Integer>();
+		blackList = new ArrayList<Integer>();
+	}
 
 
 	public void start(){
-
-		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str;
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("sampleData/id10030D.txt"));
+			//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedReader br = new BufferedReader(new FileReader("sampleData/id10030D2.txt"));
 			while((str=br.readLine())!=null){
 				list.add(str.trim());
 			}
@@ -24,28 +30,35 @@ public class Main {
 			e.printStackTrace();
 		}
 
-		int fact1 = Integer.parseInt(list.get(0));
-		int fact2 = Integer.parseInt(list.get(2));
-		numList1 = new int[fact1];
-		numList2 = new int[fact2];
 		String[] s1	=	list.get(1).split(" ");
 		String[] s2 =	list.get(3).split(" ");
 
-		for(int i=0; i<fact1; i++){
-			numList1[i] = Integer.parseInt(s1[i]);
+		for(int i=0; i<s1.length; i++){
+			numList1.add(Integer.parseInt(s1[i]));
 		}
-		for(int i=0; i<fact2; i++){
-			numList2[i] = Integer.parseInt(s2[i]);
+		for(int i=0; i<s2.length; i++){
+			numList2.add(Integer.parseInt(s2[i]));
 		}
 
-		for(int i=0; i<fact1; i++){
-			for(int j=0; j<fact2; j++){
-				if(numList1[i] == numList2[j]){
+		for(int i=0; i<numList2.size(); i++){
+			int sample = numList2.remove(i);
+
+			for(int j=0; j<numList1.size(); j++){
+				if(numList1.remove(j)== sample){
 					count++;
 				}
 			}
+
 		}
-		System.out.println(count);
+		System.out.println("Count: "+ count);
+		//test
+		for(String s : s1){
+			System.out.println(s);
+		}
+		System.out.println("-----");
+		for(String s : s2){
+			System.out.println(s);
+		}
 	}
 	public static void main(String[] args){
 		new Main().start();
