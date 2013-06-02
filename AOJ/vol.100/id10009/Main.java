b2f3a0ee1e1589825d1d410cce1b01566b8c4848
betelgeuse <betelgeuse525@gmail.com>
2013-06-03 02:45:03 +0900

    solved id10009

---
+++ AOJ/vol.100/id10009/Main.java
@@ -0,0 +1,31 @@
+package id10009;
+
+/*
+ * doubleで計算しておいて正規表現で切り取る感じ
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args){
+		new Main().round();
+	}
+	
+	void round(){
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = null;
+		try {
+			str = br.readLine();
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		double r = Double.parseDouble(str);
+		double s = r*r*Math.PI;
+		double l = 2*r*Math.PI;
+		System.out.printf("%.6f %.6f",s,l);
+		
+	}
+}
